2f512c6eb7e5c6c509b1b7497d70c97591f3a958
Kevin Adrian <kevin19305.ib@gmail.com>
2026-04-27 22:42:04 +0700

    feat: add CORS configuration and deployment documentation for production environment

--- backend-api/config/cors.php
+++ backend-api/config/cors.php
@@ -1,6 +1,6 @@
 <?php
 // config/cors.php
-header("Access-Control-Allow-Origin: *"); // Saat production, ganti * dengan domain React kamu
+header("Access-Control-Allow-Origin: http://103.23.199.184"); // Saat production, ganti * dengan domain React kamu
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
